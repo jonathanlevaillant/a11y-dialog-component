49c59f7481875068ae140bca4ba39e1449a29314
jonathanlevaillant <jonathan.levaillant@outlook.com>
2018-02-01 18:40:50 +0100

    updated demo files

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,5 +1,14 @@
 import Dialogs from './../../dist/a11y-dialog-component.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+
+  // initial config
   Dialogs.init();
+
+  // programmatically open modal
+  document.getElementById('js-trigger-dialog-2').addEventListener('click', () => {
+    Dialogs.open('dialog-2', {
+      triggerId: 'js-trigger-dialog-2',
+    });
+  });
 });
