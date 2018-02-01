e78100efc9f73e19199c1c6e36996c340f03e880
jonathanlevaillant <jonathan.levaillant@outlook.com>
2018-02-01 16:23:32 +0100

    fixed nested dialogs + custom class names

--- src/a11y-dialog-component.js
+++ src/a11y-dialog-component.js
@@ -2,6 +2,8 @@
  ========================================================================== */
 
 const Dialogs = (() => {
+  const DATA_COMPONENT = '[data-component="dialog"]';
+
   const CLASS_NAMES = {
     pageClassName: 'js-page',
     inertLayersClassName: 'js-inert-layer',
@@ -28,9 +30,9 @@ const Dialogs = (() => {
     constructor(options) {
       this.trigger = options.trigger;
       this.dialog = document.getElementById(options.dialog);
-      this.dismissTriggers = this.dialog.querySelectorAll('[data-dismiss]');
+      this.dismissTriggers = this.queryFilter(this.dialog.querySelectorAll('[data-dismiss]'));
 
-      this.focusableElements = this.dialog.querySelectorAll(FOCUSABLE_ELEMENTS);
+      this.focusableElements = this.queryFilter(this.dialog.querySelectorAll(FOCUSABLE_ELEMENTS));
       [this.firstFocusableElement] = this.focusableElements;
       this.lastFocusableElement = this.focusableElements[this.focusableElements.length - 1];
 
@@ -38,7 +40,7 @@ const Dialogs = (() => {
       this.inertLayers = document.getElementsByClassName(options.inertLayersClassName);
       this.disabledPageClassName = options.disabledPageClassName;
 
-      this.isNested = options.isNested;
+      this.disabledPage = options.disabledPage;
       this.isOpen = options.isOpen;
 
       this.open = this.open.bind(this);
@@ -49,6 +51,27 @@ const Dialogs = (() => {
       this.onAnimationEnd = this.onAnimationEnd.bind(this);
 
       if (this.isOpen) this.open();
+    }
+
+    queryFilter(selectors) {
+      const triggers = this.dialog.querySelectorAll(DATA_COMPONENT);
+      const elements = [];
+      let isValidated = true;
+
+      if (triggers.length === 0) return selectors;
+
+      selectors.forEach((selector) => {
+        triggers.forEach((trigger) => {
+          const dialog = document.getElementById(trigger.dataset.target);
+
+          if (dialog.contains(selector)) isValidated = false;
+        });
+
+        if (isValidated) elements.push(selector);
+        isValidated = true;
+      });
+
+      return elements;
     }
 
     focusTrap(event) {
@@ -77,7 +100,7 @@ const Dialogs = (() => {
 
       this.dialog.removeAttribute('aria-hidden');
 
-      if (!this.isNested) {
+      if (this.disabledPage) {
         this.page.classList.add(this.disabledPageClassName);
 
         Array.prototype.forEach.call(this.inertLayers, (inertLayer) => {
@@ -86,11 +109,11 @@ const Dialogs = (() => {
         });
       }
 
-      // setting focus on the first focusable element
-      this.setFocus();
-
       // add event listeners
       this.addEventListeners();
+
+      // setting focus on the first focusable element
+      this.setFocus();
     }
 
     close(event) {
@@ -98,7 +121,7 @@ const Dialogs = (() => {
 
       this.dialog.setAttribute('aria-hidden', true);
 
-      if (!this.isNested) {
+      if (this.disabledPage) {
         this.page.classList.remove(this.disabledPageClassName);
 
         Array.prototype.forEach.call(this.inertLayers, (inertLayer) => {
@@ -107,11 +130,11 @@ const Dialogs = (() => {
         });
       }
 
-      // restoring focus
-      if (this.trigger) this.trigger.focus();
-
       // remove event listeners
       this.removeEventListeners();
+
+      // restoring focus
+      if (this.trigger) this.trigger.focus();
     }
 
     addEventListeners() {
@@ -141,6 +164,9 @@ const Dialogs = (() => {
     }
 
     onKeydown(event) {
+      // prevent nested dialogs
+      event.stopPropagation();
+
       if (event.which === KEY_CODES.escape) this.close(event);
       if (event.which === KEY_CODES.enter && event.target.hasAttribute('data-dismiss')) this.close(event);
       if (event.which === KEY_CODES.tab) this.focusTrap(event);
@@ -159,31 +185,27 @@ const Dialogs = (() => {
     }
 
     render() {
-      if (this.trigger) {
-        this.trigger.addEventListener('click', this.open);
-        this.trigger.addEventListener('keydown', (event) => {
-          if (event.which === KEY_CODES.enter) this.open(event);
-        });
-      }
+      this.trigger.addEventListener('click', this.open);
+      this.trigger.addEventListener('keydown', (event) => {
+        if (event.which === KEY_CODES.enter) this.open(event);
+      });
     }
   }
 
   // save all active dialogs
   const activeDialogs = [];
+  let customClassNames;
 
-  const open = (dialog, config = CLASS_NAMES) => {
-    const options = Object.assign({}, CLASS_NAMES, config);
-    options.dialog = dialog;
-    options.trigger = config.trigger ? document.getElementById(config.trigger) : false;
+  const open = (dialog, { triggerId = null, disabledPage = true } = {}) => {
+    const options = { ...customClassNames, ...{ dialog, disabledPage } };
 
-    options.isNested = config.isNested || false;
+    options.trigger = document.getElementById(triggerId);
     options.isOpen = true;
 
-    const currentDialog = new Dialog(options);
-    currentDialog.render();
+    const activeDialog = new Dialog(options);
 
-    // add targeted dialog to array
-    activeDialogs.push(currentDialog);
+    // add active dialog to array
+    activeDialogs.push(activeDialog);
   };
 
   const close = (dialog) => {
@@ -191,21 +213,23 @@ const Dialogs = (() => {
       if (dialog === activeDialog.dialog.id) {
         activeDialog.close();
 
-        // remove targeted dialog from array
+        // remove closed dialog from array
         activeDialogs.splice(index, 1);
       }
     });
   };
 
-  const init = (selectors = '[data-component="dialog"]', config = CLASS_NAMES) => {
-    const triggers = document.querySelectorAll(selectors);
-    const options = Object.assign({}, CLASS_NAMES, config);
+  const init = (config = CLASS_NAMES) => {
+    const triggers = document.querySelectorAll(DATA_COMPONENT);
+    const options = { ...CLASS_NAMES, ...config };
+
+    customClassNames = { ...options };
 
     triggers.forEach((trigger) => {
       options.trigger = trigger;
       options.dialog = trigger.dataset.target;
 
-      options.isNested = trigger.dataset.nested === 'true';
+      options.disabledPage = trigger.dataset.disabledPage !== 'false';
       options.isOpen = trigger.dataset.open === 'true';
 
       const dialog = new Dialog(options);
