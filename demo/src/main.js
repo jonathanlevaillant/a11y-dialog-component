de532e468e83a10697bf0c20e041808af940ac58
jonathanlevaillant <jonathan.levaillant@outlook.com>
2018-05-10 11:34:03 +0200

    updated demo files

--- demo/src/main.js
+++ demo/src/main.js
@@ -3,12 +3,31 @@ import Dialogs from './../../dist/a11y-dialog-component.es.js';
 document.addEventListener('DOMContentLoaded', () => {
 
   // initial config
-  Dialogs.init();
-
-  // programmatically open dialog
-  document.getElementById('js-trigger-dialog-2').addEventListener('click', () => {
-    Dialogs.open('dialog-2', {
-      triggerId: 'js-trigger-dialog-2',
-    });
+  Dialogs.init({
+    disabledDocumentClass: 'is-inactive',
+    activeTriggerClass: 'is-expanded',
   });
+
+  // programmatically render dialogs
+  Dialogs.render('dialog-1', {
+    triggerId: 'dialog-trigger-1',
+    isOpen: true,
+    labelledbyId: false,
+  });
+
+  Dialogs.close('dialog-1');
+
+  Dialogs.render('dialog-tooltip-1', {
+    triggerId: 'dialog-tooltip-trigger-1',
+    hasBackdrop: false,
+    modal: false,
+    tooltip: true,
+    disableScroll: false,
+  });
+
+  Dialogs.render('dialog-3');
+
+  window.setTimeout(() => Dialogs.open('dialog-3'), 2000);
+
+  console.log(Dialogs.getDialogs());
 });
